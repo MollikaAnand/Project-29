5bced0949ecae86e2226fcb3c20c585c52013616
arfana017 <70667415+arfana017@users.noreply.github.com>
2020-11-17 00:31:57 -0800

    Create Polygon.js

---
+++ Polygon.js
@@ -0,0 +1,36 @@
+class Polygon {
+
+    constructor(x,y,diameter) {
+
+        var options = {
+            
+           restitution:0.9,
+           friction:1,
+           density:1.2
+
+        }
+
+       this.x = x;
+       this.y = y;
+       this.diameter = diameter;
+       this.body = Bodies.circle(this.x,this.y,this.diameter/2,options); 
+
+        World.add(world, this.body);
+
+    }
+
+    display() {
+
+        var pos = this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        rotate(this.body.angle);
+        rectMode(CENTER);
+        fill("pink");
+        ellipse(0,0,this.diameter,this.diameter);
+        ellipseMode(CENTER);
+        pop();
+
+    }
+
+}
